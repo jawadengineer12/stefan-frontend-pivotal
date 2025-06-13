9d63fe3c8ced6361cc05c8743760fcfc28348f38
JAFFARDEV12 <jaffar.dev12@gmail.com>
2025-06-14 02:56:16 +0500

    Update resetsuccesfully.jsx

--- src/pages/signin/resetsuccesfully.jsx
+++ src/pages/signin/resetsuccesfully.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import AuthImage from "../../components/authImage/AUTHImage";
 import { Link } from "react-router-dom";
-import LockImage from "../../components/authImage/lockImage";
+//import LockImage from "../../components/authImage/lockImage";
 
 export default function ResetSuccessfully() {
   return (
-    <div className="flex flex-col sm:flex-row min-h-screen overflow-hidden">
+    <div className="flex flex-col sm:flex-row min-h-screen overflow-hidden font-['Readex']">
       <div className="w-full lg:w-3/5 flex flex-col items-center justify-center px-6 sm:px-10 h-screen">
-        <LockImage className="w-[100px] sm:w-[133.82px] h-[100px] sm:h-[133px] mb-4" />
+        {/* <LockImage className="w-[100px] sm:w-[133.82px] h-[100px] sm:h-[133px] mb-4" /> */}
 
-        <h2 className="text-2xl sm:text-3xl font-bold text-blue-500 text-center">
+        <h2 className="text-2xl sm:text-3xl font-bold text-green-500 text-center">
           WELL DONE!
         </h2>
         <p className="text-gray-600 text-center mt-2 w-74">
@@ -18,7 +18,7 @@ export default function ResetSuccessfully() {
         </p>
 
         <Link to="/">
-          <button className="bg-blue-500 text-white px-6 py-3 rounded-lg text-lg font-semibold mt-6 cursor-pointer">
+          <button className="bg-green-500 text-white px-6 py-3 rounded-lg text-lg font-semibold mt-6 cursor-pointer">
             Sign In to Continue
           </button>
         </Link>
