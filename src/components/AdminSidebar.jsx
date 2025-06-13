47587d9ba972642c1748f4799cc5199f221d7be9
JAFFARDEV12 <jaffar.dev12@gmail.com>
2025-06-14 02:10:39 +0500

    Update AdminSidebar.jsx

--- src/components/AdminSidebar.jsx
+++ src/components/AdminSidebar.jsx
@@ -33,44 +33,44 @@ const AdminSidebar = ({ sidebarVisible, setSidebarVisible }) => {
       </button>
 
       <aside
-        className={`bg-blue-800 text-white fixed top-0 left-0 h-screen flex flex-col justify-between p-6 z-40 shadow-md transition-all duration-300 
+        className={`bg-[#345A7D] text-white fixed top-0 left-0 h-screen flex flex-col justify-between p-6 z-40 shadow-md transition-all duration-300 
           ${isOpen ? "translate-x-0" : "-translate-x-full"}
           lg:translate-x-0 ${sidebarVisible ? "lg:w-64" : "lg:w-0 lg:-translate-x-full"}`}
       >
         {sidebarVisible && (
           <>
             <div className="flex flex-col space-y-4 mt-8">
-              <h1 className="text-xl font-bold">Admin Dashboard</h1>
+              <h1 className="font-['Montserrat'] text-xl font-bold">Admin Dashboard</h1>
                <button
                 onClick={() => handleNavClick("/admin/dashboard/company")}
-                className="text-left hover:bg-blue-600 px-4 py-2 rounded cursor-pointer"
+                className="font-['Readex'] text-left hover:bg-[#7292af] px-4 py-2 rounded cursor-pointer"
               >
                 Manage Companies
               </button>
               <button
                 onClick={() => handleNavClick("/admin/dashboard/create-question")}
-                className="text-left hover:bg-blue-600 px-4 py-2 rounded cursor-pointer"
+                className="font-['Readex'] text-left hover:bg-[#7292af] px-4 py-2 rounded cursor-pointer"
               >
                 Configure Sections / Questions
               </button>
              
               <button
                 onClick={() => handleNavClick("/admin/dashboard/get-questions")}
-                className="text-left hover:bg-blue-600 px-4 py-2 rounded cursor-pointer"
+                className="font-['Readex'] text-left hover:bg-[#7292af] px-4 py-2 rounded cursor-pointer"
               >
                 See all Responses
               </button>
 
               <button
                 onClick={() => handleNavClick("/admin/dashboard/feedback")}
-                className="text-left hover:bg-blue-600 px-4 py-2 rounded cursor-pointer"
+                className="font-['Readex'] text-left hover:bg-[#7292af] px-4 py-2 rounded cursor-pointer"
               >
                 Generate Pipo Report
               </button>
             </div>
             <button
               onClick={handleLogout}
-              className="w-full bg-red-600 hover:bg-red-700 text-white mt-6 px-4 py-2 rounded cursor-pointer"
+              className="font-['Readex'] w-full bg-[#C40000] hover:bg-[#c40101c9] text-white mt-6 px-4 py-2 rounded cursor-pointer"
             >
               Logout
             </button>
