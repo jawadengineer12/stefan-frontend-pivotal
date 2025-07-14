8b1d92bf8d9bddef97bb9c12eef0f95f621828db
JAFFARDEV12 <jaffar.dev12@gmail.com>
2025-07-15 02:19:03 +0500

    Update AdminSidebar.jsx

--- src/components/AdminSidebar.jsx
+++ src/components/AdminSidebar.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import { FiMenu, FiX } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
 
 const AdminSidebar = ({ sidebarVisible, setSidebarVisible }) => {
   const navigate = useNavigate();
@@ -20,7 +20,6 @@ const AdminSidebar = ({ sidebarVisible, setSidebarVisible }) => {
 
   return (
     <>
-      
       <button
         className="block lg:hidden fixed top-4 left-4 z-50 bg-black p-2 rounded"
         onClick={() => setIsOpen(!isOpen)}
@@ -35,42 +34,48 @@ const AdminSidebar = ({ sidebarVisible, setSidebarVisible }) => {
       <aside
         className={`bg-[#345A7D] text-white fixed top-0 left-0 h-screen flex flex-col justify-between p-6 z-40 shadow-md transition-all duration-300 
           ${isOpen ? "translate-x-0" : "-translate-x-full"}
-          lg:translate-x-0 ${sidebarVisible ? "lg:w-64" : "lg:w-0 lg:-translate-x-full"}`}
+          lg:translate-x-0 ${
+            sidebarVisible ? "lg:w-64" : "lg:w-0 lg:-translate-x-full"
+          }`}
       >
         {sidebarVisible && (
           <>
             <div className="flex flex-col space-y-4 mt-8">
-              <h1 className="font-['Montserrat'] text-xl font-bold">Admin Dashboard</h1>
-               <button
+              <h1 className=" text-xl font-bold"style={{ fontFamily: "Montserrat", fontWeight: 600 }}>
+                Admin Dashboard
+              </h1>
+              <button
                 onClick={() => handleNavClick("/admin/dashboard/company")}
-                className="font-['Readex'] text-left hover:bg-[#7292af] px-4 py-2 rounded cursor-pointer"
+                className="font-['Readex_Pro'] text-left hover:bg-[#7292af] px-4 py-2 rounded cursor-pointer"
               >
                 Manage Companies
               </button>
               <button
-                onClick={() => handleNavClick("/admin/dashboard/create-question")}
-                className="font-['Readex'] text-left hover:bg-[#7292af] px-4 py-2 rounded cursor-pointer"
+                onClick={() =>
+                  handleNavClick("/admin/dashboard/create-question")
+                }
+                className="font-['Readex_Pro'] text-left hover:bg-[#7292af] px-4 py-2 rounded cursor-pointer"
               >
                 Configure Sections / Questions
               </button>
-             
+
               <button
                 onClick={() => handleNavClick("/admin/dashboard/get-questions")}
-                className="font-['Readex'] text-left hover:bg-[#7292af] px-4 py-2 rounded cursor-pointer"
+                className="font-[Readex_Pro] text-left hover:bg-[#7292af] px-4 py-2 rounded cursor-pointer"
               >
                 See all Responses
               </button>
 
               <button
                 onClick={() => handleNavClick("/admin/dashboard/feedback")}
-                className="font-['Readex'] text-left hover:bg-[#7292af] px-4 py-2 rounded cursor-pointer"
+                className="font-['Readex_Pro'] text-left hover:bg-[#7292af] px-4 py-2 rounded cursor-pointer"
               >
                 Generate Pipo Report
               </button>
             </div>
             <button
               onClick={handleLogout}
-              className="font-['Readex'] w-full bg-[#C40000] hover:bg-[#c40101c9] text-white mt-6 px-4 py-2 rounded cursor-pointer"
+              className="font-['Readex_Pro'] w-full bg-[#C40000] hover:bg-[#c40101c9] text-white mt-6 px-4 py-2 rounded cursor-pointer"
             >
               Logout
             </button>
