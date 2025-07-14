4625d7caf045fce4f7f1453948c4889543b4a077
JAFFARDEV12 <jaffar.dev12@gmail.com>
2025-07-15 02:20:31 +0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="flex items-center justify-center w-full max-w-screen-xl mx-auto px-4">
         <div className="flex items-center w-full">
           <hr className="flex-grow border-gray-300" />
-          <span className="px-4 text-gray-500 whitespace-nowrap">
+          <span className="px-4 text-gray-500 whitespace-nowrap font-[Readex_Pro]">
             2025 QuestAlre. All Rights Reserved.
           </span>
           <hr className="flex-grow border-gray-300" />
